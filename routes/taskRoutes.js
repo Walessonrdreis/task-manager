5fc54a6986ea36e130b622510eade131c6d75c1f
Walessonrdreis <wallllase@hotmail.com>
2024-10-27 21:25:57 -0300

    feat: updates add Procfile

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();// Cria uma instância do router
 
 //Definindo as rotas da API
 router.post('/tasks',taskController.addTask);
-router.put('/tasks/:id/complete',taskController.completeTask);
+router.put('/tasks/:id/complete',taskController.completedTask);
 router.get('/tasks/',taskController.listTasks);
 
 export default router;
